afae17eeea6dbd2f73912a2c4f6b4bdff60b5fe9
Manish R Jain <manishrjain@gmail.com>
2022-11-06 01:53:23 -0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
 go build . || exit
 
+echo "URL is $URL"
+if [ -z "$URL" ]; then echo "Please export URL"; exit; fi
+
 dur=60s
 
-./rpc-bench --rpc $URL --method eth_getBlockByHash --dur=$dur
-./rpc-bench --rpc $URL --method eth_getBlockByNumber --dur=$dur
-./rpc-bench --rpc $URL --method eth_getBlockTransactionCountByHash --dur=$dur
-./rpc-bench --rpc $URL --method eth_getBlockTransactionCountByNumber --dur=$dur
-./rpc-bench --rpc $URL --method eth_getTransactionByHash --dur=$dur
-./rpc-bench --rpc $URL --method eth_getTransactionReceipt --dur=$dur
+./rpc-bench --rpc=$URL --method=eth_getBlockByHash --dur=$dur
+./rpc-bench --rpc=$URL --method=eth_getBlockByNumber --dur=$dur
+./rpc-bench --rpc=$URL --method=eth_getBlockTransactionCountByHash --dur=$dur
+./rpc-bench --rpc=$URL --method=eth_getBlockTransactionCountByNumber --dur=$dur
+./rpc-bench --rpc=$URL --method=eth_getTransactionByHash --dur=$dur
+./rpc-bench --rpc=$URL --method=eth_getTransactionReceipt --dur=$dur
