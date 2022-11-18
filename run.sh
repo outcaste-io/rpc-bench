787525141ed2ba12d09b1c00055dea293e731c8c
Manish R Jain <manishrjain@gmail.com>
2022-11-18 19:06:17 +0000

    dirs

--- run.sh
+++ run.sh
@@ -4,10 +4,11 @@ echo "URL is $URL"
 if [ -z "$URL" ]; then echo "Please export URL"; exit; fi
 
 dur=60s
+sample=100
 
-./rpc-bench --rpc=$URL --method=eth_getBlockByHash --dur=$dur
-./rpc-bench --rpc=$URL --method=eth_getBlockByNumber --dur=$dur
-./rpc-bench --rpc=$URL --method=eth_getBlockTransactionCountByHash --dur=$dur
-./rpc-bench --rpc=$URL --method=eth_getBlockTransactionCountByNumber --dur=$dur
-./rpc-bench --rpc=$URL --method=eth_getTransactionByHash --dur=$dur
-./rpc-bench --rpc=$URL --method=eth_getTransactionReceipt --dur=$dur
+./rpc-bench --rpc=$URL --method=eth_getBlockByHash --dur=$dur --sample=$sample
+./rpc-bench --rpc=$URL --method=eth_getBlockByNumber --dur=$dur --sample=$sample
+./rpc-bench --rpc=$URL --method=eth_getTransactionByHash --dur=$dur --sample=$sample
+./rpc-bench --rpc=$URL --method=eth_getTransactionReceipt --dur=$dur --sample=$sample
+./rpc-bench --rpc=$URL --method=eth_getBlockTransactionCountByHash --dur=$dur --sample=$sample
+./rpc-bench --rpc=$URL --method=eth_getBlockTransactionCountByNumber --dur=$dur --sample=$sample
