1a76b76a53beb32596883baf61c245f8b69732fc
Manish R Jain <manishrjain@gmail.com>
2022-11-18 11:17:31 -0800

    add secret

--- run.sh
+++ run.sh
@@ -6,9 +6,9 @@ if [ -z "$URL" ]; then echo "Please export URL"; exit; fi
 dur=60s
 sample=100
 
-./rpc-bench --rpc=$URL --method=eth_getBlockByHash --dur=$dur --sample=$sample
-./rpc-bench --rpc=$URL --method=eth_getBlockByNumber --dur=$dur --sample=$sample
-./rpc-bench --rpc=$URL --method=eth_getTransactionByHash --dur=$dur --sample=$sample
-./rpc-bench --rpc=$URL --method=eth_getTransactionReceipt --dur=$dur --sample=$sample
-./rpc-bench --rpc=$URL --method=eth_getBlockTransactionCountByHash --dur=$dur --sample=$sample
-./rpc-bench --rpc=$URL --method=eth_getBlockTransactionCountByNumber --dur=$dur --sample=$sample
+./rpc-bench --rpc=$URL --secret=$SECRET --method=eth_getBlockByHash --dur=$dur --sample=$sample
+./rpc-bench --rpc=$URL --secret=$SECRET --method=eth_getBlockByNumber --dur=$dur --sample=$sample
+./rpc-bench --rpc=$URL --secret=$SECRET --method=eth_getTransactionByHash --dur=$dur --sample=$sample
+./rpc-bench --rpc=$URL --secret=$SECRET --method=eth_getTransactionReceipt --dur=$dur --sample=$sample
+./rpc-bench --rpc=$URL --secret=$SECRET --method=eth_getBlockTransactionCountByHash --dur=$dur --sample=$sample
+./rpc-bench --rpc=$URL --secret=$SECRET --method=eth_getBlockTransactionCountByNumber --dur=$dur --sample=$sample
